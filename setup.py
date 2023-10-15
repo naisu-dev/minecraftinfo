1c099976e1314a1ca0de1a16a56010d14361921b
naisu-dev <118091656+naisu-dev@users.noreply.github.com>
2023-10-15 15:56:39 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='minecraftinfo',
-    version="1.3",
+    version="1.5",
     description="Request information Minecraft JavaEdition and BedrockEdition",
     author='naisu',
     packages=find_packages(),
