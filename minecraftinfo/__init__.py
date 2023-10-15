014045daa4877269620e35e2376613d78046afbc
naisu-dev <118091656+naisu-dev@users.noreply.github.com>
2023-10-15 15:24:24 +0900

    Add files via upload

--- minecraftinfo/__init__.py
+++ minecraftinfo/__init__.py
@@ -1,3 +1,3 @@
-from .main import mcje_server
+from .main import mcje_server, get_skin, get_cape
 
-__all__ = ["mcje_server"]
+__all__ = ["mcje_server", "get_skin", "get_cape"]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='minecraftinfo',
-    version="1.1",
+    version="1.2",
     description="Request information Minecraft JavaEdition and BedrockEdition",
     author='naisu',
     packages=find_packages(),
