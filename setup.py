d71aef0ada96688ea4e8e564fdb2bdfc67f3874b
naisu-dev <118091656+naisu-dev@users.noreply.github.com>
2023-10-23 15:49:34 +0900

    バージョン更新

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as fp:
+    readme = fp.read()
+LONG_DESCRIPTION = readme
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+
 setup(
     name='minecraftinfo',
-    version="1.9",
+    version="1.11",
     description="Request information Minecraft JavaEdition and BedrockEdition",
     author='naisu',
     packages=find_packages(),
-    license='MIT'
+    license='MIT',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE
 )
